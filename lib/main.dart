ea33e1c22274a5b0ba449813b25cdbb043b2bc2c
Cheahy Lao <cheahylao5p@gmail.com>
2023-02-25 21:59:36 +0700

    get rid of useless shiet

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:class_assessment_app/pages/splash_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
