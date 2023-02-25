6660e5e6091bd8f574ab76fec424ff5bddb7ef59
Cheahy Lao <cheahylao5p@gmail.com>
2023-02-25 21:41:32 +0700

    profile page done

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,3 +1,6 @@
+import 'package:class_assessment_app/pages/home_page.dart';
+import 'package:class_assessment_app/pages/quizzes.dart';
+import 'package:class_assessment_app/pages/signin.dart';
 import 'package:flutter/material.dart';
 import '../widgets/lesson.dart';
 
@@ -13,10 +16,175 @@ class _ProfileState extends State<Profile> {
         Container(
           color: Colors.blueGrey[700],
           width: double.infinity,
+          
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: const <Widget>[
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('assets/images/logo.png'),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Text(
+                        'John Doe',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          color: Colors.blueGrey[700],
+          width: double.infinity,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text('profile')],
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.5,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            color: Colors.red[100],
+                            padding: EdgeInsets.all(5),
+                            child: Icon(
+                              Icons.heart_broken,
+                              color: Colors.red,
+                              size: 20,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: InkWell(
+                              onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Quizzes(),
+                                ),
+                              ),
+                              child: Text(
+                                'My Quizzes',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.5,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            color: Colors.yellow[100],
+                            padding: EdgeInsets.all(5),
+                            child: Icon(
+                              Icons.bookmark,
+                              color: Colors.yellow[900],
+                              size: 20,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: InkWell(
+                              onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MyHomePage(),
+                                ),
+                              ),
+                              child: Text(
+                                'My Classroom',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          color: Colors.green[100],
+                          padding: EdgeInsets.all(5),
+                          child: Icon(
+                            Icons.logout,
+                            color: Colors.green[900],
+                            size: 20,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SignIn(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Logout',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ],
